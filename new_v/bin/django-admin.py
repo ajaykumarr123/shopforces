f891cc3e297bd301d3049e60d9ad14cc405982c5
ajaykumarr123 <ajaymeena3210@gmail.com>
2020-06-01 22:08:29 +0530

    soft. project

--- new_v/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ajay/Desktop/final shop_management/shop_management/new_v/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
