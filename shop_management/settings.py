e1734116c3fdecdd8ca7fd896b228b7920ab1f1e
ajaykumarr123 <ajaymeena3210@gmail.com>
2021-06-18 16:41:40 +0530

    heroku deployed

--- shop_management/settings.py
+++ shop_management/settings.py
@@ -134,10 +134,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS= (
 
-    os.path.join(BASE_DIR ,'assets'),
+    os.path.join(BASE_DIR ,'static'),
 )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
 
 
 CART_SESSION_ID = 'cart'
