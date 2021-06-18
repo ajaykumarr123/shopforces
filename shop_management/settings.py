83f9b4568e131d7b4e8fd5916bbd992fe2d21705
ajaykumarr123 <ajaymeena3210@gmail.com>
2021-06-18 17:10:01 +0530

    heroku deployed

--- shop_management/settings.py
+++ shop_management/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS= (
